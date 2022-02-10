cc0dfa7d8b5ccc6b6456a1ad288474f3ec952b29
sindhu <botlasindhu@gmail.com>
2022-02-10 23:42:58 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
+// import necessary dependencies 
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom'; 
+import './styles.css';
+import Lion from './lion.jpg';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  let Gem="SHARE" 
+  let Cham="LEARN MORE"
+// create a component
+    function Hello(){ 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return( 
+    <>
+    <div className='myself'> 
+      <div className='one'> <img   src={Lion} alt=""  />  
+        <h2>Lion</h2> 
+        <p>The lion (Panthera leo) is a large cat of the genus Panthera <br></br>native to Africa and India. It has a muscular, deep-chested body, short,<br></br>rounded head, round ears, and a hairy tuft at the end of its tail.</p>
+      <h4>{Gem}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{Cham}</h4>
+      
+       </div>
+      </div> 
+      </>
+        )
+    }
+
+// show it to user 
+ ReactDOM.render(  
+  // what do you want to display
+  <div>
+  <Hello/>
+   </div>,
+// where do you want display
+document.getElementById('root')
+
+ );
